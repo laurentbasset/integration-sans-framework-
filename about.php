7c0316b2cf9f8850c9205f5620d3619ae6651cf1
laurentbasset <l.basset@codeur.online>
2021-08-16 15:45:54 +0200

    mise en place architecture globale

---
+++ about.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- script -->
+    <script type="text/javascript" src="js/main.js"></script>
+
+    <!-- styles CSS -->
+    <link  href="css/styles.css" rel="stylesheet">
+
+    <title>Le Crédit Lédonien : à propos</title>
+</head>
+<body>
+    <header></header>
+    <main></main>
+    <footer></footer>
+    
+</body>
+</html>
