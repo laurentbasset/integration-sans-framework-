0a5bd7debee624b55318cafa84e86d91fc40441b
FReaper39 <a.elaji@codeur.online>
2021-08-18 15:58:02 +0200

    ajout wireframe footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,20 @@
-<em>footer</em>
+<div id="itemFooter1">
+            <div id="wrapLogoWhite">
+                <img src="img/imageCL.jpg">
+            </div>
+            <p>Un crédit vous engage et doit être remboursé.<br> 
+            Vérifiez vos capacités de remboursementavant de vous engager.</p>
+</div>
+        <div id="itemFooter2">
+            <p> 1 place de la République <br>
+            39000 Lons le Saunier<br>
+            Tél : + 33 (0) 3 84 12 56 89 <br>
+            contact@lecreditledonien.fr<br> 
+                
+            
+        </div>
+        <div id="itemFooter3"><p> Mentions légales <br>
+                Politique de confidentialité <br>
+                Une réalisation<a href="https://www.accesscodeschool.fr/" target="blank" id="Acslons"> ACS Lons le Saunier</a>
+        </div>
+                
