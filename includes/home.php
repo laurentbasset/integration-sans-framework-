f56a6c17807d8662f942ad52bf03e9d29f024ef7
laurentbasset <l.basset@codeur.online>
2021-08-18 15:42:25 +0200

    corrections

--- includes/home.php
+++ includes/home.php
@@ -1,5 +1,3 @@
-<main id="main">
-    <h2>Accueil</h2>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam nesciunt, aliquid dolorem quis repellat cum incidunt quos ex veritatis, obcaecati adipisci nemo distinctio, est corporis rem eaque. Numquam, officiis eum?</p>
-    <a href="?page=about" title="à propos" >à propos</a>
-</main>
+<h2>Accueil</h2>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam nesciunt, aliquid dolorem quis repellat cum incidunt quos ex veritatis, obcaecati adipisci nemo distinctio, est corporis rem eaque. Numquam, officiis eum?</p>
+<a href="?page=about" title="à propos" >à propos</a>
