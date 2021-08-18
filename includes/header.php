1bd70522f0fa564055cfb1e16519abfad58ab56a
FReaper39 <a.elaji@codeur.online>
2021-08-18 16:02:06 +0200

    ajout wireframe header

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,8 @@
-<h1>Header</h1>
+<div id="leftHeader">
+    <div id="wrpLogo"><img src="img/imageCL.jpg" alt="logo"></div>
+</div>
+
+<div id="rightHeader">
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis, tenetur.<br>
+Dolores aut ipsa dignissimos tempora!
+</div>
