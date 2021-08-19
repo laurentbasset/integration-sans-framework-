ddd8dae4baa48e850b5802a6066d03e461523ff0
YJouille <y.j.39110@gmail.com>
2021-08-19 10:47:14 +0200

    update test Yamina

--- includes/home.php
+++ includes/home.php
@@ -1,3 +1,55 @@
-<h2>Modification Accueil</h2>
+<!-- <h2>Accueil</h2>
 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam nesciunt, aliquid dolorem quis repellat cum incidunt quos ex veritatis, obcaecati adipisci nemo distinctio, est corporis rem eaque. Numquam, officiis eum?</p>
-<a href="?page=about" title="à propos" >à propos</a>
+<a href="?page=about" title="à propos" >à propos</a> -->
+
+<article id="articleHome1" class="flex-container">
+            <section class="flex-item-left blocTxt">
+                <h2>Prêt Personnel multi-projets </h2>
+                <p class="offre"><span class="taux"> 4,07%</span> TAEG fixe<br>
+                Offre valable jusqu'au 31/08/2021</p>
+                <p class="duree">Pour 6 500€ empruntés sur 60 mois</p>
+                <button class=" button btnDecouvrir ">Découvrir</button>
+            </section>
+            <section class="flex-item-right"></section>
+        </article>
+
+        <article id="articleHome2" class="flex-container">
+            <section class="flex-item-left blocTxt">
+                <h2>Comment se déroule une demande de crédit chez le Crédit Lédonien ? </h2>
+                <p class="description">
+                    <span class="txtBold">Prêt personnel :</span> pour une demande de crédit à la consommation de plus de <span class="txtBold">5000 euros</span>. Sélectionnez ensuite le projet qui vous concerne, le montant, la durée et lancez le simulateur de crédit. Voici les différents types de projet que <span class="txtBold">le Crédit Lédonien</span> vous propose de financer via un prêt personnel : voiture et moto neuve ou d'occasion ; travaux, mobilier et décoration ; véhicule de loisirs pour un camping-car, une caravane ou un bateau et projet divers pour un événement de vie (un mariage, une naissance, une fête de famille, un divorce ou des funérailles) ou le financement des études supérieures de vos enfants, de voyages, de loisirs ou de votre santé.
+                </p>
+                <p class="description">
+                    <span class="txtBold">Crédit renouvelable</span> : pour une somme d'argent jusqu’à <span class="txtBold">3000 euros</span> que vous pouvez utiliser sans justificatif dans l'ensemble du réseau Mastercard, en boutique comme sur Internet,. Sélectionnez ensuite le montant de votre choix entre <span class="txtBold">500</span> et <span class="txtBold">3000 euros</span> et la durée de remboursement souhaitée, puis cliquez sur simuler.
+                </p>
+                <p class="description">
+                    <span class="txtBold">Rachat de crédit :</span> si vous souhaitez regrouper un ou plusieurs crédits. Ne prenez jamais un crédit à la consommation pour rembourser un crédit existant ! En tant qu'organisme de crédit responsable, <span class="txtBold">le Crédit Lédonien</span> vous accompagne et vous propose une offre de rachat et de regroupement de crédits adaptée à votre situation afin de vous permettre d'honorer vos échéances dans le respect de votre budget.
+                </p>  
+            </section>
+            <section class="flex-item-right"></section>
+        </article>
+
+        <article id="articleHome3" class="flex-container">
+            <section class="flex-item-left">
+                <figure class="wrpImgMonnaie"><img src="img/money-2724235_1920.jpg" alt="image monnaie"></figure>
+               
+            </section>
+            <section class="flex-item-right blocTxt">
+                <h2>Le crédit responsable par le Crédit Lédonien</h2>
+                <p class="description">
+                    Être <span class="txtBold">responsable…</span><br>
+                    …cela peut paraître évident pour <span class="txtBold">le Crédit Lédonien</span> qui prône le crédit responsable depuis plus de <span class="txtBold">20</span> ans.Cette posture qui nous amène à évoluer constamment au gré des aspirations et modes de vie des Français, a toujours été portée par des actes.
+                </p>
+                <p class="description">
+                    Elle revêt trois dimensions complémentaires. Tout d’abord, la <span class="txtBold">responsabilité</span> pour vous, en proposant des solutions de financement et des services qui permettent une <span class="txtBold">complète maîtrise de son crédit et de son budget</span>.
+                    Cette responsabilité s’exprime également pour nous, à travers une politique des ressources humaines <span class="txtBold">inclusive</span> et <span class="txtBold">solidaire</span>, favorisant l’<span class="txtBold">épanouissement des collaborateurs</span>.
+                </p>
+                <p class="description">
+                    Enfin, une responsabilité pour tous, car nous sommes tous concernés. Nos modules d’éducation financière et notre présence dans le <span class="txtBold">monde éducatif et professionnel</span> sont les parfaits exemples de cet engagement.
+                </p>
+                <p class="description">
+                    Au service de nos clients, nous facilitons l’accès à une consommation plus responsable et plus durable. Alors soyons toujours plus responsables, ensemble.
+                </p>
+                <button class="button">En savoir plus</button>  
+            </section>
+        </article>
