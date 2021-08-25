e58672cfdde560fbe679f8c6e3bdedd40936e3b8
FReaper39 <a.elaji@codeur.online>
2021-08-25 14:53:09 +0200

    OverlayFinish

--- js/main.js
+++ js/main.js
@@ -7,9 +7,13 @@ document.getElementById('btCloseCalcul').addEventListener("click", function() {
   document.getElementById('simCalculPret').style.display = 'none';
 });
 
-document.getElementById("bt").addEventListener("click", function(){
+document.getElementById("btOpenMenu").addEventListener("click", function(){
 document.getElementById("mySidenav").style.width = "100%";
+document.getElementById("btOpenMenu").style.display = "none";
+document.getElementById("btCloseMenu").style.display = "inline-block";
 });
-document.getElementById("btClose").addEventListener("click", function(){
-document.getElementById("mySidenav").style.width = "0";
+document.getElementById("btCloseMenu").addEventListener("click", function(){
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("btOpenMenu").style.display = "inline-block";
+  document.getElementById("btCloseMenu").style.display = "none";
 });
