3d983ced886945ee1519cd9ad539c723a51f9909
laurentbasset <l.basset@codeur.online>
2021-08-26 16:17:36 +0200

    reglages z-index nav et simulateur

--- js/main.js
+++ js/main.js
@@ -11,10 +11,21 @@ document.getElementById('field1').addEventListener("change", function() {
   alert('hello');
 });
 
-function gotoSlide2(){
-  let root = document.documentElement;
-  //root.style.setProperty('--animSlide', 'anim_slide2' );
-  root.style.setProperty('--pos0', '-400px' );
-  root.style.setProperty('--pos1', '-800px' );
-  root.style.setProperty('--pos2', '0px' );
-}
+// ouverture fermeture menu lateral
+document.getElementById("btOpenMenu").addEventListener("click", function(){
+document.getElementById("mySidenav").style.width = "100%";
+document.getElementById("btOpenMenu").style.display = "none";
+document.getElementById("btCloseMenu").style.display = "inline-block";
+});
+document.getElementById("btCloseMenu").addEventListener("click", function(){
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("btOpenMenu").style.display = "inline-block";
+  document.getElementById("btCloseMenu").style.display = "none";
+});
+// function gotoSlide2(){
+//   let root = document.documentElement;
+//   //root.style.setProperty('--animSlide', 'anim_slide2' );
+//   root.style.setProperty('--pos0', '-400px' );
+//   root.style.setProperty('--pos1', '-800px' );
+//   root.style.setProperty('--pos2', '0px' );
+// }
