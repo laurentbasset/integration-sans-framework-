e7511ba153279304671e80db0dda4ec1a60c558c
laurentbasset <l.basset@codeur.online>
2021-08-27 16:46:27 +0200

    divers reglages

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Venez simulez un pret personnel à hauteur de 500€ minimum cher crédit Lédonien.">
 
     <!-- script -->
     <script type="text/javascript" src="js/main.js" async></script>
