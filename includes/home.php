e2962ffcc8864dfe34bfe52d5241b2e464faf880
laurentbasset <l.basset@codeur.online>
2021-08-27 13:44:25 +0200

    reglages polices + simulateur responsive

--- includes/home.php
+++ includes/home.php
@@ -43,20 +43,20 @@
         <article class="articleHome flex-container">
             <section class="flex-item-left blocTxtHome">
                 <h2>Comment se déroule une demande de crédit chez le Crédit Lédonien ? </h2>
-                <p >
+                <p class="txt">
                     <strong>Prêt personnel :</strong> pour une demande de crédit à la consommation de plus de <strong>5000 euros</strong>. Sélectionnez ensuite le projet qui vous concerne, le montant, la durée et lancez le simulateur de crédit. Voici les différents types de projet que <strong>le Crédit Lédonien</strong> vous propose de financer via un prêt personnel : voiture et moto neuve ou d'occasion ; travaux, mobilier et décoration ; véhicule de loisirs pour un camping-car, une caravane ou un bateau et projet divers pour un événement de vie (un mariage, une naissance, une fête de famille, un divorce ou des funérailles) ou le financement des études supérieures de vos enfants, de voyages, de loisirs ou de votre santé.
                 </p>
-                <p >
+                <p class="txt">
                     <strong>Crédit renouvelable</strong> : pour une somme d'argent jusqu’à <strong>3000 euros</strong> que vous pouvez utiliser sans justificatif dans l'ensemble du réseau Mastercard, en boutique comme sur Internet,. Sélectionnez ensuite le montant de votre choix entre <strong>500</strong> et <strong>3000 euros</strong> et la durée de remboursement souhaitée, puis cliquez sur simuler.
                 </p>
-                <p >
+                <p class="txt">
                     <strong>Rachat de crédit :</strong> si vous souhaitez regrouper un ou plusieurs crédits. Ne prenez jamais un crédit à la consommation pour rembourser un crédit existant ! En tant qu'organisme de crédit responsable, <strong>le Crédit Lédonien</strong> vous accompagne et vous propose une offre de rachat et de regroupement de crédits adaptée à votre situation afin de vous permettre d'honorer vos échéances dans le respect de votre budget.
                 </p>  
             </section>
             <section class="flex-item-right" id="services">
                 <div id="topServices">
                     <h2>Découvrez nos offres et services</h2>
-                    <p>Auto, Travaux, Déco, le <strong>Crédit Lédonien</strong> propose une solution de financement pour chacun de vos projets personnels.</p>
+                    <p >Auto, Travaux, Déco, le <strong>Crédit Lédonien</strong> propose une solution de financement pour chacun de vos projets personnels.</p>
                 </div>
                 <div id="bottomServices">
                     <div id="leftBottomServices">
@@ -84,18 +84,18 @@
          
             <section class="flex-item-right blocTxtHome">
                 <h2>Le crédit responsable par le Crédit Lédonien</h2>
-                <p>
+                <p class="txt">
                     Être <strong>responsable…</strong><br>
                     …cela peut paraître évident pour <strong>le Crédit Lédonien</strong> qui prône le crédit responsable depuis plus de <strong>20</strong> ans.Cette posture qui nous amène à évoluer constamment au gré des aspirations et modes de vie des Français, a toujours été portée par des actes.
                 </p>
-                <p>
+                <p class="txt">
                     Elle revêt trois dimensions complémentaires. Tout d’abord, la <strong>responsabilité</strong> pour vous, en proposant des solutions de financement et des services qui permettent une <strong>complète maîtrise de son crédit et de son budget</strong>.
                     Cette responsabilité s’exprime également pour nous, à travers une politique des ressources humaines <strong>inclusive</strong> et <strong>solidaire</strong>, favorisant l’<strong>épanouissement des collaborateurs</strong>.
                 </p>
-                <p>
+                <p class="txt">
                     Enfin, une responsabilité pour tous, car nous sommes tous concernés. Nos modules d’éducation financière et notre présence dans le <strong>monde éducatif et professionnel</strong> sont les parfaits exemples de cet engagement.
                 </p>
-                <p>
+                <p class="txt">
                     Au service de nos clients, nous facilitons l’accès à une consommation plus responsable et plus durable. Alors soyons toujours plus responsables, ensemble.
                 </p>
                 <button class="button">En savoir plus</button>  
