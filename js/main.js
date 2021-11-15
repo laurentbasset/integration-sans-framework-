907204993d89782ec42603f114794e4a9f2900b6
Aleaddine El Aji <87655501+AleaddineElAji@users.noreply.github.com>
2021-11-15 14:37:49 +0100

    blockQuiChange

--- js/main.js
+++ js/main.js
@@ -1,31 +1,123 @@
 document.getElementById('btPretPerso').addEventListener("click", function() {
-    document.getElementById('simMenu').style.display = 'none';
-    document.getElementById('simCalculPret').style.display = 'block';
-  });
+  document.getElementById('simMenu').style.display = 'none';
+  document.getElementById('simCalculPret').style.display = 'block';
+});
 document.getElementById('btCloseCalcul').addEventListener("click", function() {
-  document.getElementById('simMenu').style.display = 'block';
-  document.getElementById('simCalculPret').style.display = 'none';
+document.getElementById('simMenu').style.display = 'block';
+document.getElementById('simCalculPret').style.display = 'none';
 });
 
-document.getElementById('field1').addEventListener("change", function() {
-  alert('hello');
+
+/// Calcul simulateur
+let sim = document.getElementById('simulateur');
+let arrInputs = sim.getElementsByTagName('input');
+for (let i = 0; i < arrInputs.length; i++) {
+arrInputs[i].addEventListener("input", function () {
+  switch (this.id) {
+    case 'amount': inputAmount(this); break;
+    case 'monthPay': inputMonthPay(this);break;
+    case 'term':inputTerm(this);break;
+  }
+  checkInput();
+});
+}
+function inputAmount(amount) { 
+//Recalculate month pay
+    if(arrInputs[2].value !=0){
+        arrInputs[1].value = Math.round(amount.value / arrInputs[2].value);   
+    }
+  }
+
+function inputMonthPay(monthPay) {
+//Recalculate terms
+    if(monthPay.value != 0){
+      arrInputs[2].value = Math.round(arrInputs[0].value / monthPay.value);
+    }
+}
+
+function inputTerm(term) {
+//Recalculate month pay
+    if(term.value !=0){
+      arrInputs[1].value = Math.round(arrInputs[0].value / term.value);
+    }
+}
+function checkInput(){
+
+if (arrInputs[0].value < 500 || arrInputs[0].value > 75000){
+    arrInputs[0].classList.add('unvalid');
+  }else{
+    arrInputs[0].classList.remove('unvalid');
+}
+
+if (arrInputs[1].value < 10 || arrInputs[1].value > 12000){ 
+    arrInputs[1].classList.add('unvalid');
+  }else{
+    arrInputs[1].classList.remove('unvalid'); 
+}
+
+if (arrInputs[2].value < 6 || arrInputs[2].value > 84){ 
+    arrInputs[2].classList.add('unvalid');
+  }else{
+    arrInputs[2].classList.remove('unvalid');
+}
+
+}
+
+// ouverture/fermeture menu lateral
+let btToggleMenu = document.getElementById("btOpenMenu");
+btToggleMenu.addEventListener("click", function(){
+if (btToggleMenu.className=="active") {
+  document.getElementById('menuOverlay').style.width = "0";
+  btToggleMenu.classList.remove('active');
+}else{
+  document.getElementById('menuOverlay').style.width = "100%";
+  btToggleMenu.classList.add('active');
+}
 });
 
-// ouverture fermeture menu lateral
-document.getElementById("btOpenMenu").addEventListener("click", function(){
-  document.getElementById("mySidenav").style.width = "100%";
-  document.getElementById("btOpenMenu").style.display = "none";
-  document.getElementById("btCloseMenu").style.display = "inline-block";
+document.getElementById('call').addEventListener('click', function() { 
+window.open("tel:+330384125689");
+}
+);
+
+//Affichage leftBottomServices
+
+divVisuel = document.getElementById('visuel')
+divTxtVisuel = document.getElementById('txtVisuel')
+idImg = document.getElementById('imgDefault')
+
+document.getElementById('choix1').addEventListener("click", function() {
+idImg.style.display = 'none'
+let lastChild = divVisuel.lastChild;
+divVisuel.removeChild(lastChild);
+img1 = document.createElement('img');
+img1.src = "img/creditAuto.jpg"
+divVisuel.appendChild(img1)
 });
-document.getElementById("btCloseMenu").addEventListener("click", function(){
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("btOpenMenu").style.display = "inline-block";
-  document.getElementById("btCloseMenu").style.display = "none";
+
+document.getElementById('choix2').addEventListener("click", function() {
+idImg.style.display = 'none'
+let lastChild = divVisuel.lastChild;
+divVisuel.removeChild(lastChild);
+img1 = document.createElement('img');
+img1.src = "img/credit.jpg"
+divVisuel.appendChild(img1)
 });
-// function gotoSlide2(){
-//   let root = document.documentElement;
-//   //root.style.setProperty('--animSlide', 'anim_slide2' );
-//   root.style.setProperty('--pos0', '-400px' );
-//   root.style.setProperty('--pos1', '-800px' );
-//   root.style.setProperty('--pos2', '0px' );
-// }
+
+document.getElementById('choix3').addEventListener("click", function() {
+idImg.style.display = 'none'
+let lastChild = divVisuel.lastChild;
+divVisuel.removeChild(lastChild);
+img1 = document.createElement('img');
+img1.src = "img/creditCard.jpg"
+divVisuel.appendChild(img1)
+});
+
+document.getElementById('choix4').addEventListener("click", function() {
+idImg.style.display = 'none'
+let lastChild = divVisuel.lastChild;
+divVisuel.removeChild(lastChild);
+img1 = document.createElement('img');
+img1.src = "img/projet.jpg"
+divVisuel.appendChild(img1)
+});
